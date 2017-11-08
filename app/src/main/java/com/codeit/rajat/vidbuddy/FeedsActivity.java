e8335b6fe3fd1778e511580c6222d9caa2d5288e
iamrajatroy <rajatr62@gmail.com>
2017-11-08 23:20:20 +0530

    User photo added on top left of feeds activity. Try to implement toolbar

--- app/src/main/java/com/codeit/rajat/vidbuddy/FeedsActivity.java
+++ app/src/main/java/com/codeit/rajat/vidbuddy/FeedsActivity.java
@@ -1,21 +1,32 @@
 package com.codeit.rajat.vidbuddy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
 
 public class FeedsActivity extends AppCompatActivity {
 
     private Button logoutBtn;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private String userName;
+    private DatabaseReference dbRef;
+    private LinearLayout detailsLayout;
+    private ImageView userPhoto;
+    private TextView textViewUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +41,23 @@ public class FeedsActivity extends AppCompatActivity {
                 }
             }
         };
-        logoutBtn = (Button)findViewById(R.id.logout_button);
+        logoutBtn = findViewById(R.id.logout_button);
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
             }
         });
+
+        userName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        Uri photoUri = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl();
+
+        detailsLayout = findViewById(R.id.details_layout);
+        userPhoto = findViewById(R.id.user_photo);
+        textViewUserName = findViewById(R.id.user_name);
+        textViewUserName.setText(userName);
+        Picasso.with(getApplicationContext()).load(photoUri)
+                .into(userPhoto);
     }
 
     @Override
